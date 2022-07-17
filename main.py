0b8f630cc03638d0b05518fc47cb754418d4516e
Ketan Chowdhury <ketanchowdhury@protonmail.com>
2022-07-17 12:49:48 +0530

    Added more colors to the print statements
    Basic code done. Creating pull request and next task is to work with the apis

--- main.py
+++ main.py
@@ -27,35 +27,35 @@ REDDIT_SECRET = os.getenv("REDDIT_SECRET")
 sleep_1()
 
 if  TWITTER_API_KEY == "":
-    rich.print("❗ Twitter API Key is not provided in .env file")
+    rich.print("❗ [bold cyan]Twitter[/bold cyan] [bold red]API Key is not provided in .env file[/bold red]")
     rich.print("Check .env file if you have entered the API Key")
     TWITTER_API_KEY_EXISTS = "false"
 else:
     TWITTER_API_KEY_EXISTS = "true"
 
 if TWITTER_API_SECRET == "":
-    rich.print("❗ Twitter API Secret is not provided in .env file")
+    rich.print("❗ [bold cyan]Twitter[/bold cyan] [bold red]API Secret is not provided in .env file[/bold red]")
     rich.print("Check .env file if you have entered the API Secret")
     TWITTER_API_SECRET_EXISTS = "false"
 else:
     TWITTER_API_SECRET_EXISTS = "true"
 
 if TWITTER_BEARER_TOKEN == "":
-    rich.print("❗ Twitter Bearer Token is not provided in .env file")
+    rich.print("❗ [bold cyan]Twitter[/bold cyan] [bold red]Bearer Token is not provided in .env file[/bold red]")
     rich.print("Check .env file if you have entered the Bearer Token")
     TWITTER_BEARER_TOKEN_EXISTS = "false"
 else:
     TWITTER_BEARER_TOKEN_EXISTS = "true"
 
 if REDDIT_API_KEY == "":
-    rich.print("❗ Reddit API Key is not provided in .env file")
+    rich.print("❗ [bold orange3]Reddit[/bold orange3] [bold red]API Key is not provided in .env file[/bold red]")
     rich.print("Check .env file if you have entered the API Key")
     REDDIT_API_KEY_EXISTS = "false"
 else:
     REDDIT_API_KEY_EXISTS = "true"
 
 if REDDIT_SECRET == "":
-    rich.print("❗ Reddit Secret is not provided in .env file")
+    rich.print("❗ [bold orange3]Reddit[/bold orange3] [bold red]Secret is not provided in .env file[/bold red]")
     rich.print("Check .env file if you have entered the Secret")
     REDDIT_SECRET_EXISTS = "false"
 else:
@@ -64,7 +64,7 @@ else:
 if TWITTER_API_KEY_EXISTS == "false" and TWITTER_API_SECRET_EXISTS == "false" and TWITTER_BEARER_TOKEN_EXISTS == "false" and REDDIT_API_KEY_EXISTS == "false" and REDDIT_SECRET_EXISTS == "false":
     rich.print("❗ API Keys are not provided in the .env file")
     rich.print("Check .env file and enter values before running main.py")
-    rich.print("Exiting...")
+    rich.print("[bold red]Exiting...[/bold red]")
     exit()
 elif TWITTER_API_KEY_EXISTS == "true" and TWITTER_API_SECRET_EXISTS == "true" and TWITTER_BEARER_TOKEN_EXISTS == "true" and REDDIT_API_KEY_EXISTS == "true" and REDDIT_SECRET_EXISTS == "true":
     rich.print('|--------------------------------------------------------------------|')
